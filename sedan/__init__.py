bb90cb558a93c5be5843ab74804ffc2f7e9f554e
jay groven <jgroven@lutsendata.com>
2012-02-25 19:36:04 -0600

    add a nice readme and a license

---
+++ sedan/__init__.py
@@ -0,0 +1,8 @@
+from couchdbkit.exceptions import ResourceConflict
+from couchdbkit.exceptions import ResourceNotFound
+from .exceptions           import CreateScheduled
+from .exceptions           import UpdateScheduled
+from .exceptions           import OverwriteScheduled
+from .exceptions           import DeleteScheduled
+from .batch                import CouchBatch
+
